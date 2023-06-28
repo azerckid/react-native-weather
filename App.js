8b6aa0c02b43dca0bb1209f7de501e94631ba3e1
남형석 <namhyeongseog@azerc-m2-MacBook-Air.local>
2023-06-28 17:42:08 +0900

    1. before ICONS

--- App.js
+++ App.js
@@ -1,11 +1,88 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import * as Location from "expo-location";
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Dimensions,
+  ActivityIndicator,
+} from "react-native";
+
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
+const API_KEY = "45847c92c4b17407254d3d738d721454";
 
 export default function App() {
+  const [location, setLocation] = useState();
+  const [ok, setOk] = useState(true);
+  const [city, setCity] = useState("...Loading");
+
+  const [days, setDays] = useState([]);
+
+  const getWeather = async () => {
+    try {
+      const { granted } = await Location.requestForegroundPermissionsAsync();
+      if (!granted) {
+        setOk(false);
+      }
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+      setLocation({ latitude, longitude });
+      const currLocation = await Location.reverseGeocodeAsync(
+        { latitude, longitude },
+        { useGoogleMaps: false }
+      );
+      setCity(currLocation[0].region);
+
+      fetch(
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}`
+      )
+        .then((response) => response.json())
+        .then((json) => {
+          // console.log(json.daily);
+          setDays(json.daily);
+        });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getWeather();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      <View style={styles.city}>
+        <Text style={styles.cityname}>{city}</Text>
+      </View>
+      <ScrollView
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}
+      >
+        {days.length === 0 ? (
+          <View style={styles.day}>
+            <ActivityIndicator color="white" size="large" />
+          </View>
+        ) : (
+          days.map((day, index) => (
+            <View key={index} style={styles.day}>
+              <Text style={styles.temp}>
+                {parseFloat(day.temp.day - 273.15).toFixed(0)}°
+              </Text>
+              <Text style={styles.desc}>{day.weather[0].main}</Text>
+              <Text style={styles.description}>
+                {day.weather[0].description}
+              </Text>
+            </View>
+          ))
+        )}
+      </ScrollView>
     </View>
   );
 }
@@ -13,8 +90,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "dodgerblue",
+  },
+  city: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityname: {
+    fontSize: 60,
+    fontWeight: "500",
+  },
+  weather: {},
+  day: {
+    width: SCREEN_WIDTH,
+    alignItems: "center",
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 150,
+  },
+  desc: {
+    marginTop: -30,
+    fontSize: 40,
+    color: "white",
+  },
+  description: {
+    color: "white",
   },
 });
---
+++ README.md
@@ -0,0 +1,27 @@
+```
+expo login => npx expo login
+```
+
+```
+expo init => npx create-expo-app 앱이름
+cd 앱이름
+```
+
+```
+npx expo install react-dom react-native-web @expo/webpack-config
+```
+
+```
+npx expo start
+```
+
+```
+https://reactnative.dev/
+https://reactnative.directory/
+https://docs.expo.dev/versions/latest/
+https://snack.expo.dev/
+```
+
+```
+npx expo install expo-location
+```
